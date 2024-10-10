a5a20f34c47cfdeb4104c72754455ad440caf716
Asad Sarwar <101384237+asadsarwar1@users.noreply.github.com>
2024-10-10 17:10:12 +0500

    certificates added

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -3,6 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import CertificateCards from "./CertificateCards";
 import FEDevHackerrank from "../../Assets/frontend_developer_react.pdf";
+import JSInterHackerrank from "../../Assets/js_intermediate.pdf";
+import SEHackerrank from "../../Assets/se.pdf";
 
 function Certificates() {
   return (
@@ -27,6 +29,26 @@ function Certificates() {
           </Col>
           <Col md={4} className="project-card">
             <CertificateCards
+              docSource={SEHackerrank}
+              isBlog={false}
+              title="Software Engineer Certificate"
+              description="Awarded by HackerRank for successfully completing the Software Engineer Certificate. The certificate verifies that the recipient has successfully completed the Software Engineer Certificate."
+              openLink="https://www.hackerrank.com/certificates/56fb13932891"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <CertificateCards
+              docSource={JSInterHackerrank}
+              isBlog={false}
+              title="JavaScript (Intermediate) Certificate"
+              description="Awarded by HackerRank for successfully completing the JavaScript (Intermediate) Certificate. The certificate verifies that the recipient has successfully completed the JavaScript (Intermediate) Certificate."
+              openLink="https://www.hackerrank.com/certificates/7c5552aeb986"
+            />
+          </Col>
+        </Row>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <CertificateCards
               imgPath="https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~GMTR8AY2YPRP/CERTIFICATE_LANDING_PAGE~GMTR8AY2YPRP.jpeg"
               isBlog={false}
               title="GCP: Core Infrastructure"
@@ -43,8 +65,6 @@ function Certificates() {
               openLink="https://www.credly.com/badges/b73c48de-d683-4d48-8f92-09fc59235554/linked_in_profile"
             />
           </Col>
-        </Row>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <CertificateCards
               imgPath="https://s3.amazonaws.com/coursera_assets/meta_images/generated/CERTIFICATE_LANDING_PAGE/CERTIFICATE_LANDING_PAGE~SX6KPVQHKM29/CERTIFICATE_LANDING_PAGE~SX6KPVQHKM29.jpeg"
